81a266c9131b52cffee9e4c4617f3016df2fa963
chooblarin <choo.bla.rin@gmail.com>
2018-06-12 13:27:59 +0900

    Demo about input properties

--- checkerboard.js
+++ checkerboard.js
@@ -1,13 +1,25 @@
 registerPaint('checkerboard', class {
-  paint(ctx, geom, _props) {
-    const colors = ['#ff5752', '#38aed7', '#ffffff'];
-    const size = 32;
+
+  static get inputProperties() {
+    return ['--checkerboard-spacing', '--checkerboard-size'];
+  }
+
+  paint(ctx, geom, properties) {
+    const sizeProp = properties.get('--checkerboard-size');
+    const spacingProp = properties.get('--checkerboard-spacing');
+
+    // As of now, mostly wrappers around strings,
+    const size = parseInt(sizeProp.toString());
+    const spacing = parseInt(spacingProp.toString());
+
+    const colors = ['#ff5752', '#38aed7'];
+
     for (let y = 0; y < geom.height / size; y += 1) {
       for (let x = 0; x < geom.width / size; x += 1) {
         const c = colors[(x + y) % colors.length];
         ctx.beginPath();
         ctx.fillStyle = c;
-        ctx.rect(x * size, y * size, size, size);
+        ctx.rect(x * (size + spacing), y * (size + spacing), size, size);
         ctx.fill();
       }
     }
